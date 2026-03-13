583b7c4709e5e5ad313a0c457d7a2f400b82765c
sumeet <sumeetsonawane997@gmail.com>
2026-03-13 17:20:45 +0530

    Revert NativeWind to v2 and remove worklets

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,21 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
+    presets: ["babel-preset-expo"],
     plugins: [
+      "nativewind/babel",
       [
         "module-resolver",
         {
           root: ["./"],
           alias: {
-            "@": "./",
-          },
-        },
+            "@": "./"
+          }
+        }
       ],
-      "react-native-reanimated/plugin",
-    ],
+      "react-native-reanimated/plugin"
+    ]
   };
 };
